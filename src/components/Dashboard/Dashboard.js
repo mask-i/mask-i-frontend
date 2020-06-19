f99e61056592994851355687c810cb7840b47f6d
mnai01 <imatlak03@gmail.com>
2020-06-19 06:20:23 -0400

    Made it mobile responsive

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,18 +1,19 @@
-import React from "react";
-import { Typography, Grid } from "@material-ui/core";
-import CameraFeed from "../CameraFeed/CameraFeed";
-import ActivityLog from "../ActivityLog/ActivityLog";
-import classes from "./Dashboard.module.css";
+import React from 'react';
+import { Typography, Grid } from '@material-ui/core';
+import CameraFeed from '../CameraFeed/CameraFeed';
+import ActivityLog from '../ActivityLog/ActivityLog';
+import classes from './Dashboard.module.css';
 const Dashboard = () => {
   return (
     <div>
       <Grid container className={classes.mainGrid}>
-        <Grid item xs={6} style={{ backgroundColor: "white" }}>
-          <Typography variant="h5">CAMERA FEED</Typography>
+        <Grid item xs={12} md={6} style={{ backgroundColor: 'white' }}>
+          <Typography variant='h5'>CAMERA FEED</Typography>
           <CameraFeed />
         </Grid>
-        <Grid item xs={6} style={{ backgroundColor: "white" }}>
-          <Typography variant="h5">ACTIVITY LOG</Typography>
+        <div className={classes.middle}></div>
+        <Grid item xs={12} md={6} style={{ backgroundColor: 'white' }}>
+          <Typography variant='h5'>ACTIVITY LOG</Typography>
           <ActivityLog />
         </Grid>
       </Grid>
